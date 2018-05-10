2b7f37fe0549ad18c4d2a020fb308945f8e2e2cc
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-05-10 23:30:10 +1000

    simplifying browser list to just "> 1%"

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -42,7 +42,7 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
         ]
       }).on('error', plugins.sass.logError))
       .pipe(plugins.postcss([
-        autoprefixer({browsers: ['last 2 version', '> 1%', 'ie >= 11'], grid: true }),
+        autoprefixer({browsers: ['> 1%'], grid: true }),
         px2rem(px2rem_settings)
       ]))
       .pipe(plugins.rename(function(path) {
